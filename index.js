f8e59aac641f6dfb915e7ec7ea9516af2b88ed73
Stephen <stevriss22@gmail.com>
2021-09-27 23:32:23 -0400

    commit tree a5d34a1b30ca487b42bb2db8b2e7daf9bf6e0507

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function addingEventListener() {
     } );
 
 }
+
+// addingEventListener function grabs the input tag by its ID('input') and then adds an event listener to it
+//input.addEventListener takes a 'click' event and an anonymous callback function
+//the  callback function triggers an aler that says 'iwas clicked!' when clicked
